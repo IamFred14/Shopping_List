75bfbc7f3f99f4578a0b6f1d5d47e9549667efd4
IamFred14 <fredericopinto7@gmail.com>
2021-09-12 20:27:06 +0100

    Adicionar arquivos de projeto.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonAddEntry_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(entryBox.Text) && !shpnList.Items.Contains(entryBox.Text))
+            {
+                shpnList.Items.Add(entryBox.Text);
+                entryBox.Clear();
+            }
+        }
+        private void ButtonDeleteEntry_Click(object sender, RoutedEventArgs e)
+        {
+            shpnList.Items.Clear();
+        }
+    }
+
+}
